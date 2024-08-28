03e6e85f63945044eb452faf5f2bf09cebd0fb2a
rinngo <73386339+rinngo0302@users.noreply.github.com>
2024-08-28 15:46:43 +0900

    set Rigidbody and use AddForce()

--- Assets/Script/Edamame/Edamame.cs
+++ Assets/Script/Edamame/Edamame.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Edamame : MonoBehaviour
 {
+    // Rigidbody
+    private Rigidbody rb;
+
     // 定数
     const float G = 9.8f * 0.1f; // 重力
 
@@ -19,40 +23,42 @@ public class Edamame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        PushObj();
+    }
+
+    private void PushObj()
+    {
+        float rad = TranslateAngleToRad(_angle);
+
+        float deltaX = Mathf.Sin(rad * Time.deltaTime) * _speedX;
+        float deltaY = _speedY;
+        float deltaZ = Mathf.Cos(rad * Time.deltaTime) * _speedZ;
+
+        rb.AddForce(new Vector3(deltaX, deltaY, deltaZ), ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
         _time += Time.deltaTime;
-
-
-        if (!isOnTable())
-        {
-            float rad = translateAngleToRad(_angle);
-
-            float deltaX = Mathf.Sin(rad * Time.deltaTime) * _speedX;
-            float deltaZ = Mathf.Cos(rad * Time.deltaTime) * _speedZ;
-            float deltaY = _speedY - (G * _time * _time) / 2;// Rigidbodyに変更予定
-            
-            this.transform.Translate(new Vector3(deltaX, deltaY, deltaZ));
-        }
     }
 
-    // テーブル(？)の上に乗る(条件)
-    // isOnTriggerに変更するかもしれない
-    bool isOnTable()
-    {
-        return transform.position.y < 0;
-    }
-
-    public void setAngle(float angle)
+    /// <summary>
+    /// 枝豆の角度を設定する
+    /// </summary>
+    /// <param name="angle"> 角度 </param>
+    public void SetAngle(float angle)
     {
         _angle = angle;
     }
 
-    float translateAngleToRad(float angle)
+    /// <summary>
+    /// 角度法を弧度法に変換
+    /// </summary>
+    /// <param name="angle"> 角度 </param>
+    /// <returns></returns>
+    float TranslateAngleToRad(float angle)
     {
         return Mathf.PI / (180 / angle);
     }
