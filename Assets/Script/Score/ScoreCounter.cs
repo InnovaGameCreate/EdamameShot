4eaf8c77aa5f0ea9646b3591dc3bcf5d233a6355
oono265 <kazumaohno55@gmail.com>
2024-09-02 22:48:00 +0900

    金豆のスコア
    金豆が籠に入ったときに呼び出すメソッド。スコアをプラス5にする。

---
+++ Assets/Script/Score/ScoreCounter.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scorecounter : MonoBehaviour
+{
+    [SerializeField] private int score;
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void ScoreUp()
+    {
+        //金豆以外が籠に入ったときのメソッド
+        score = score + 1;
+    }
+    // Start is called before the first frame update
+    public int GetScore()
+    {
+        return score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
