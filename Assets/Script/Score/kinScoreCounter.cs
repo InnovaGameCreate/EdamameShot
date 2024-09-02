9fe69fc7baf705ea55c613aed144ad7219260182
oono265 <kazumaohno55@gmail.com>
2024-09-02 22:57:26 +0900

    金豆のスコア加算
    金豆が籠に入ったときに呼び出すメソッド。この時スコアを５点足す。

--- Assets/Script/Score/kinScoreCounter.cs
+++ Assets/Script/Score/kinScoreCounter.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class kinScoreCounter : MonoBehaviour
+public class kinscorecounter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int score;
     void Start()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void ScoreUp()
+    {
+        //‹à“¤‚ªâÄ‚É“ü‚Á‚½‚Æ‚«‚Ìƒƒ\ƒbƒh
+        score = score + 5;
+    }
+    // Start is called before the first frame update
+    public int GetScore()
+    {
+        return score;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
